529a8f9536b4c1de0c52b5fbb0be5e871b536b79
malaz <malaz.hatim@gmail.com>
2023-08-11 07:27:46 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+	{
+		if (i == 'q' || i == 'e')
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
