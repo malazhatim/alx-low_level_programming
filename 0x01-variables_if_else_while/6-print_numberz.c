a18429ca223c45b98f2bd0760a2065349ffe182c
malaz <malaz.hatim@gmail.com>
2023-08-11 07:47:50 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int i ;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		putcharI(i);
+	}
+	putchar('\n');
+	return (0);
+}
