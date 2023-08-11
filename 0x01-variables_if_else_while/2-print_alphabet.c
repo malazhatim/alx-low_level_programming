a59f1e939174f165807b10993b3e28e4faaa0286
malaz <malaz.hatim@gmail.com>
2023-08-11 07:06:39 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+		putchar(a '\n');
+	return (0);
+}
