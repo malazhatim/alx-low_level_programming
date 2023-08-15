95ced61274316821d688ac79e576abb428791420
malaz <malaz.hatim@gmail.com>
2023-08-15 07:38:35 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - times table function
+ *
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			int c = b * a;
+
+			if (b == 0)
+			{
+				_putchar('0');
+			} else if (c <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			} else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
