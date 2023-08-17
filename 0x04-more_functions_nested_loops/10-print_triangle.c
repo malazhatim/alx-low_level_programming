fe8f5ac305b77205e386770b16fedef18191ae03
malaz <malaz.hatim@gmail.com>
2023-08-17 09:34:56 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ *
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size - a ; b > 0; b--)
+			{
+				_putchar(' ');
+			}
+			for (c = 0; c < a; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
