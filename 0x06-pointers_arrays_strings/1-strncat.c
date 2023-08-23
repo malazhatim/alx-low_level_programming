194728c9c3359628217c7e5fa4ec593b7f0155df
malaz <malaz.hatim@gmail.com>
2023-08-23 09:24:00 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[a] != '\0')
 		a++;
 
-	while (src[b] != '\0' && j < n)
+	while (src[b] != '\0' && b < n)
 	{
 		dest[a] = src[b];
 		a++;
