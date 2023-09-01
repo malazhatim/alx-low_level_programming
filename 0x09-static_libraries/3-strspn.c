640bae3e0418ca0daadc6abc36f1d710cb6f242f
malaz <malaz.hatim@gmail.com>
2023-09-01 07:34:50 -0700

    0

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b, c, f;
+
+	c = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		f = 0;
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				c++;
+				f = 1;
+			}
+		}
+		if (f == 0)
+		{
+			return (c);
+		}
+	}
+
+	return (0);
+}
