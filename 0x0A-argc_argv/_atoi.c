66d923c799ebab96236d1a4b5c94fca863117d53
malaz <malaz.hatim@gmail.com>
2023-09-01 08:31:00 -0700

    3

--- 0x0A-argc_argv/_atoi.c
+++ 0x0A-argc_argv/_atoi.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -44,30 +43,4 @@ int _atoi(char *s)
 		return (0);
 
 	return (n);
-}
-
-/**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
-int main(int argc, char *argv[])
-{
-	int r, a, b;
-
-	if (argc < 3 || argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	a = _atoi(argv[1]);
-	b = _atoi(argv[2]);
-	r = a * b;
-
-	printf("%d\n", r);
-
-	return (0);
 }
