5f83f3a0db1a9c7237888b5a34d43f18ab6a598e
malaz <malaz.hatim@gmail.com>
2023-09-11 07:39:56 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, a1, a2;
-	dog_t *ne-dog;
+	dog_t *nedog;
 	char *b, *c;
 
 	a1 = 0;
@@ -20,30 +20,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	a2 = 0;
 	for (i = 0; owner[i] != '\0'; i++)
 		a2++;
-	ne-dog = malloc(sizeof(dog_t));
-	if (ne-dog == NULL)
+	nedog = malloc(sizeof(dog_t));
+	if (nedog == NULL)
 		return (NULL);
 	b = malloc((a1 + 1) * sizeof(char));
 	if (b == NULL)
 	{
-		free(ne-dog);
+		free(nedog);
 		return (NULL);
 	}
 	for (i = 0; i < a1; i++)
 		b[i] = name[i];
 	b[i] = '\0';
-	ne-dog->name = b;
-	ne-dog->age = age;
+	nedog->name = b;
+	nedog->age = age;
 	c = malloc((a2 + 1) * sizeof(char));
 	if (c == NULL)
 	{
 		free(b);
-		free(ne-dog);
+		free(nedog);
 		return (NULL);
 	}
 	for (i = 0; i < a2; i++)
 		c[i] = owner[i];
 	c[i] = '\0';
-	ne-dog->owner = c;
-	return (ne-dog);
+	nedog->owner = c;
+	return (nedog);
 }
