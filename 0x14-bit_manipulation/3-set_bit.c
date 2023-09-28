64240667fb2c6c33763d6405a046bb0e78169b24
malaz <malaz.hatim@gmail.com>
2023-09-28 09:06:19 -0700

    3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit -  find the value of a bit at a given index.
+ * @n: decimal number
+ * @index: index starting from 0 of the bit you want to get
+ * Return: value of a bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64) 
+		return (-1)
+
+	*n = *n | (1 << index);
+
+return (1);
+}
