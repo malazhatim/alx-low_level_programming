3ad0d3fedafffd9b8e7bc71699a94dce3f02f361
malaz <malaz.hatim@gmail.com>
2023-10-02 15:58:51 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (c = 0; text_content[c];)
 			c++;
-	}
-
-	if (a == -1 || b == -1)
+		if (a == -1 || b == -1)
 		return (-1);
+	}
 
 	close(a);
 
