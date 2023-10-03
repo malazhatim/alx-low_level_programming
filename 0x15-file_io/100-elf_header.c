c9f6592db15f017411c5d74956cbdf8f67e6850d
malaz <malaz.hatim@gmail.com>
2023-10-02 18:09:41 -0700

    4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,102 +1,4 @@
-#include "main.h"
-
-/**
- * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- * Description: If the file is not an ELF file - exit code 98.
- */
-void check_elf(unsigned char *e_ident)
-{
-	int a;
-
-	for (a = 0; a < 4; a++)
-	{
-		if (e_ident[a] != 127 &&
-		    e_ident[a] != 'E' &&
-		    e_ident[a] != 'L' &&
-		    e_ident[a] != 'F')
-		{
-			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-			exit(98);
-		}
-	}
-}
-
-/**
- * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- * Description: Magic numbers are separated by spaces.
- */
-void print_magic(unsigned char *e_ident)
-{
-	int b;
-
-	printf("  Magic:   ");
-
-	for (b = 0; b < EI_NIDENT; b++)
-	{
-		printf("%02x", e_ident[b]);
-
-		if (b == EI_NIDENT - 1)
-			printf("\n");
-		else
-			printf(" ");
-	}
-}
-
-/**
- * print_class - Prints the class of an ELF header.
- * @e_ident: A pointer to an array containing the ELF class.
- */
-void print_class(unsigned char *e_ident)
-{
-	printf("  Class:                             ");
-	if (e_ident[EI_CLASS] == ELFCLASSNONE)
-		printf("none\n");
-	else if (e_ident[EI_CLASS] == ELFCLASS32)
-		printf("ELF32\n");
-	else if (e_ident[EI_CLASS] == ELFCLASS64)
-		printf("ELF64\n");
-	else
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-}
-
-/**
- * print_data - Prints the data of an ELF header.
- * @e_ident: A pointer to an array containing the ELF class.
- */
-void print_data(unsigned char *e_ident)
-{
-	printf("  Data:                              ");
-	if (e_ident[EI_DATA] == ELFDATANONE)
-		printf("none\n");
-	else if (e_ident[EI_DATA] == ELFDATA2LSB)
-		printf("2's complement, little endian\n");
-	else if (e_ident[EI_DATA] == ELFDATA2MSB)
-		printf("2's complement, big endian\n");
-	else
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-}
-
-/**
- * print_version - Prints the version of an ELF header.
- * @e_ident: A pointer to an array containing the ELF version.
- */
-void print_version(unsigned char *e_ident)
-{
-	printf("  Version:                           %d",
-	       e_ident[EI_VERSION]);
-
-	switch (e_ident[EI_VERSION])
-	{
-	case EV_CURRENT:
-		printf(" (current)\n");
-		break;
-	default:
-		printf("\n");
-		break;
-	}
-}
+#include "mai.h"
 
 /**
  * print_osabi - Prints the OS/ABI of an ELF header.
@@ -243,7 +145,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (close(c) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c);
-		exit(98); 
+		exit(98);
 	}
 	return (0);
 }
