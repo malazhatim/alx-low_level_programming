5ea89f322dcf774017ab4af5a7bcda5c64b9e929
malazhatim <malaz.hatim@gmail.com>
2023-10-30 12:18:59 -0700

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+* main - copies the content of a file to another file.
+* @argc: number of command line arguments
+* @argv: An array containing the program command line arguments
+* Return: Always success.
+*/
+int main(int argc, char *argv[])
+{
+	int op_f, op_t, w;
+	char buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97); }
+	op_f = open(argv[1], O_RDONLY);
+	if (argv[1] == NULL || op_f < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
+
+	op_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (op_t < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(op_f);
+		exit(99); }
+	while ((w = read(op_f, buff, 1024)) > 0)
+	{
+		if (w != write(op_t, buff, w))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(op_f);
+			close(op_t);
+			exit(99);
+	}}
+	if (w < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
+	if (close(op_f) < 0)
+		print_error(op_f);
+	if (close(op_t) < 0)
+		print_error(op_t);
+return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,23 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* print_error - print file close error.
+* @file_des: file descriptor
+*
+* Return: None.
+*/
+
+void print_error(int file_des)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_des);
+	exit(100);
+}
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
