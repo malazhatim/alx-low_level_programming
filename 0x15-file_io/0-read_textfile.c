bb31577005779ecb9a8697b0154aef3dc5475ad6
malazhatim <malaz.hatim@gmail.com>
2023-10-30 10:48:06 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,30 +10,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t op, re, wr;
 	char *buff;
+	int re, wr;
+	int op;
 
 	if (filename == NULL)
 		return (0);
 
-	buff = malloc(sizeof(char) * letters);
+	op = open(filename, O_RDONLY);
+	if (op < 0)
+		return (0);
+
+	buff = malloc(letters * sizeof(char));
 	if (buff == NULL)
 		return (0);
 
-	op = open(filename, O_RDONLY);
-	if (op > 0)
-		return (0);
 	re = read(op, buff, letters);
-	wr = write(STDOUT_FILENO, buff, re);
-
-	if (op == -1 || re == -1 || wr == -1 || wr != re)
+	if (re < 0)
 	{
 		free(buff);
 		return (0);
 	}
-
-	free(buff);
+	buff[re] = '\0';
 	close(op);
 
-	return (wr);
+	wr = write(STDOUT_FILENO, buff, re);
+	free(buff);
+
+	if (wr < 0)
+		return (0);
+
+return (wr);
 }
