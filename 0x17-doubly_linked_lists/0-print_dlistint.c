dbea505a61b387993d49484545fdcb58bf68a3fb
malazhatim <malaz.hatim@gmail.com>
2023-12-07 12:02:46 -0800

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,20 @@
-#include "list.h"
-
+#include "lists.h"
 /**
- * print_dlistint - print element in list
- * @h:struct list
- * Return:list len
+ * print_dlistint - prints length and elements
+ * @h: head of list
+ * Return: size_t value
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int a;
-	size_t l = 0;
+	const dlistint_t *t;
+	int cou = 0;
 
-	for (a = 0; h; a++)
+	t = h;
+	while (t != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		b++;
+		printf("%d\n", t->n);
+		t = t->next;
+		cou++;
 	}
-	return (b);
+	return (cou);
 }
