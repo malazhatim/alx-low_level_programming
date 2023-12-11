17ef86bbc3f77e4457d3a085d9c5f8be2eaa2d5d
malazhatim <malaz.hatim@gmail.com>
2023-12-11 10:26:08 -0800

    0

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -5,47 +5,46 @@
 /**
  * main - Generates and prints passwords for the crackme5 executable.
  * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
+ * @argv: An array of pointers to the arguments
  * Return: Always 0.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char password[7], *codex;
-	int len = strlen(argv[1]), i, tmp;
+	char password[7], *code;
+	int len = strlen(argv[1]), a, t;
 
-	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	code = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	tmp = (len ^ 59) & 63;
-	password[0] = codex[tmp];
+	t = (len ^ 59) & 63;
+	password[0] = codex[t];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += argv[1][i];
-	password[1] = codex[(tmp ^ 79) & 63];
+	t = 0;
+	for (a = 0; a < len; a++)
+		t += argv[1][a];
+	password[1] = code[(t ^ 79) & 63];
 
-	tmp = 1;
-	for (i = 0; i < len; i++)
-		tmp *= argv[1][i];
-	password[2] = codex[(tmp ^ 85) & 63];
+	t = 1;
+	for (a = 0; a < len; a++)
+		t *= argv[1][a];
+	password[2] = code[(t ^ 85) & 63];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
+	t = 0;
+	for (a = 0; a < len; a++)
 	{
-		if (argv[1][i] > tmp)
-			tmp = argv[1][i];
+		if (argv[1][a] > t)
+			t = argv[1][a];
 	}
-	srand(tmp ^ 14);
-	password[3] = codex[rand() & 63];
+	srand(t ^ 14);
+	password[3] = code[rand() & 63];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += (argv[1][i] * argv[1][i]);
-	password[4] = codex[(tmp ^ 239) & 63];
+	t = 0;
+	for (a = 0; a < len; a++)
+		t += (argv[1][a] * argv[1][a]);
+	password[4] = code[(t ^ 239) & 63];
 
-	for (i = 0; i < argv[1][0]; i++)
-		tmp = rand();
-	password[5] = codex[(tmp ^ 229) & 63];
+	for (a = 0; a < argv[1][0]; a++)
+		t = rand();
+	password[5] = code[(t ^ 229) & 63];
 
 	password[6] = '\0';
 	printf("%s", password);
